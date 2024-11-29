67f153954098d6a4931ef8a1692d38737c7cab52
sakura <yjh1670814592@outlook.com>
2024-11-29 09:58:39 +0800

    test

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
           type="text"
           value={inputText}
           onChange={(e) => setInputText(e.target.value)}
-          placeholder="请输入要排序的文字"
+          placeholder="请输入要排序的文字!"
         />
         <button onClick={handleSort}>排序</button>
         {sortedText && <div className="result">排序结果: {sortedText}</div>}
